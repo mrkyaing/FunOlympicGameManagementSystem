f4ccc80c57d6b35012a1baeee3a8912908dcbfd5
mrkyaing <mr.kyaing7@gmail.com>
2023-06-27 01:52:39 +0630

    update admin dashboard.

---
+++ FunOlympicGameManagementSystem/Controllers/AdminHomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FunOlympicGameManagementSystem.Controllers {
+    public class AdminHomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
