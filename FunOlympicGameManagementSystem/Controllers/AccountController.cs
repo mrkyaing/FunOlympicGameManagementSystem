5fcebf8736e9915f8a688eeb9ea4b2d7e3fe27af
mrkyaing <mr.kyaing7@gmail.com>
2023-06-27 22:53:26 +0630

    add log info at account controller.

--- FunOlympicGameManagementSystem/Controllers/AccountController.cs
+++ FunOlympicGameManagementSystem/Controllers/AccountController.cs
@@ -2,7 +2,12 @@
 
 namespace FunOlympicGameManagementSystem.Controllers {
     public class AccountController : Controller {
+        private readonly ILogger<AccountController> _logger;
+        public AccountController(ILogger<AccountController> logger) {
+            _logger = logger;
+        }
         public IActionResult Login() {
+            _logger.LogInformation("login page loading");
             return View();
         }
 
--- FunOlympicGameManagementSystem/Controllers/HomeController.cs
+++ FunOlympicGameManagementSystem/Controllers/HomeController.cs
@@ -4,8 +4,8 @@ using System.Diagnostics;
 
 namespace FunOlympicGameManagementSystem.Controllers {
     public class HomeController : Controller {
+        
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger) {
             _logger = logger;
         }
