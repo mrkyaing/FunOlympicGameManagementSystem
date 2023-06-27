3fd72e7d3219c1e512c3149b244b0261046c83ce
mrkyaing <mr.kyaing7@gmail.com>
2023-06-28 02:03:37 +0630

    update ui.

---
+++ FunOlympicGameManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using FunOlympicGameManagementSystem.Models.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunOlympicGameManagementSystem.Controllers {
+    public class UserController : Controller {
+        private readonly ILogger<UserController> _logger;
+        private readonly AppDbContext _appDbContext;
+
+        public UserController(ILogger<UserController> logger, AppDbContext appDbContext) {
+            _logger = logger;
+            _appDbContext = appDbContext;
+        }
+        // GET: UserController
+        public ActionResult List() {
+            var users=_appDbContext.Users;
+            return View(users);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
